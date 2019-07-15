e384393f19b604bd4a71ed554ead178a99b4aab3
Anna W <anski.anna@gmail.com>
2019-07-15 16:27:51 +0200

    GET and POST works

--- server.js
+++ server.js
@@ -14,22 +14,47 @@ function getAccountById(id){
     return db('accounts').where({ id });
 }
 
+function createNewAccount({ name, budget }){
+    return db('accounts').insert({ name, budget })
+}
 
+function updateAccountById(id, {name, budget}){
+    return db('accounts').where({ id }).update({ name, budget })
+}
 server.get('/', async (req, res) => {
     res.json("success!")
 })
 
-server.get('/api/accounts', async (req, res) => {
+server.get('/accounts', async (req, res) => {
     // pull all accounts from db
     const accounts = await getAllAccounts();
     // send accounts back to client
     res.json(accounts);
   });
 
-server.get('/api/accounts/:id', async (req, res) => {
+server.get('/accounts/:id', async (req, res) => {
     const account = await getAccountById(req.params.id);
     res.json(account[0])
 })
 
+server.post('/accounts', async (req,res, next) => {
+    try {
+        const newAccountId = await createNewAccount(req.body);
+        const arrayOfUsers = await getAccountById(newAccountId[0]);
+        res.status(201).json(arrayOfUsers);
+      } catch (error) {
+        next(new Error("Couldn't create user :("));
+      }
+})
+
+server.put('/accounts/:id', async (req, res) => {
+    try {
+        const { name, budget } = req.body;
+        const result = await updateAccountById(req.params.id, { name, budget });
+        res.status(200).json(result);
+    } catch {
+        res.status(500).json({message: "could not delete account"})
+    }
+})
 
 module.exports = server;
